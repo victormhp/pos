89cdb9f1a56c20052c2b82ba44dc8aee62c74aa1
victormhp <victoralbertomhp@gmail.com>
2025-03-21 21:34:41 -0600

    Add react scan

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import './styles/global.css';
+import { scan } from 'react-scan';
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from '@/components/theme-provider';
 import { routeTree } from './routeTree.gen.ts';
+
+scan({enabled: true})
 
 export const queryClient = new QueryClient();
 
